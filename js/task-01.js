2f55b14137e5e8201dad9c20048d2885289678d2
Gniewomir368 <79970452+Gniewomir368@users.noreply.github.com>
2023-03-29 12:50:19 +0200

    Poprawki

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,25 @@
-const numOfCat = document.querySelectorAll("li.item");
-console.log(`Number of categories: ${numOfCat.length}`);
+// const numOfCat = document.querySelectorAll("li.item");
+// console.log(`Number of categories: ${numOfCat.length}`);
 
-const nameOfFirstCat = document.body.children[6].firstElementChild.childNodes[1].innerText;
-console.log(`Category: ${nameOfFirstCat}`);
-const firstElemLen = document.body.children[6].firstElementChild.childNodes[1].nextElementSibling.children;
-console.log(`Elements: ${firstElemLen.length}`);
+// const nameOfFirstCat = document.body.children[6].firstElementChild.childNodes[1].innerText;
+// console.log(`Category: ${nameOfFirstCat}`);
+// const firstElemLen = document.body.children[6].firstElementChild.childNodes[1].nextElementSibling.children;
+// console.log(`Elements: ${firstElemLen.length}`);
 
-const nameOfSecondCat = document.body.children[6].children[1].childNodes[1].innerText;
-console.log(`Category: ${nameOfSecondCat}`);
-const secondElemLen = document.body.children[6].children[1].childNodes[1].nextElementSibling.children;
-console.log(`Elements: ${secondElemLen.length}`);
+// const nameOfSecondCat = document.body.children[6].children[1].childNodes[1].innerText;
+// console.log(`Category: ${nameOfSecondCat}`);
+// const secondElemLen = document.body.children[6].children[1].childNodes[1].nextElementSibling.children;
+// console.log(`Elements: ${secondElemLen.length}`);
 
-const nameOfThirdCat = document.body.children[6].lastElementChild.childNodes[1].innerText;
-console.log(`Category: ${nameOfThirdCat}`);
-const thirdElemLen = document.body.children[6].lastElementChild.childNodes[1].nextElementSibling.children;
-console.log(`Elements: ${thirdElemLen.length}`);
+// const nameOfThirdCat = document.body.children[6].lastElementChild.childNodes[1].innerText;
+// console.log(`Category: ${nameOfThirdCat}`);
+// const thirdElemLen = document.body.children[6].lastElementChild.childNodes[1].nextElementSibling.children;
+// console.log(`Elements: ${thirdElemLen.length}`);
+
+const numberOfCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+numberOfCategories.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
-const ingrList = document.querySelectorAll(".ingredients");
-console.log(ingrList)
-for (i = 0; i < ingredients.length; i += 1) {
-  const list= document.createElement("li")
-  list.textContent = ingredients[i]
-  document.body.children[7].append(list)
-  console.log(list)
+const ingrList = document.querySelector("#ingredients");
+console.log(ingrList);
+for (let i = 0; i < ingredients.length; i += 1) {
+  const list = document.createElement("li");
+  list.textContent = ingredients[i];
+  ingrList.append(list);
+  console.log(list);
 }
-console.log(document.body)
+console.log(document.body);
